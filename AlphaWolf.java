d71b243163adbd26e3b787bf26d8ed3a2929f3a8
VitalijsTickovs <v.tickovs@student.maastrichtuniversity.nl>
2024-03-06 18:36:46 +0100

    In order to run the app: javac Wolves.java javac AlphaWolf.java The second is very important!!!

--- AlphaWolf.java
+++ AlphaWolf.java
@@ -1,27 +1,62 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AlphaWolf implements Wolf{
-
-    private boolean isLeader;
+    private boolean isLeader = false;
     private final static BlackBoard blackBoard = new BlackBoard();
 
-    public AlphaWolf(boolean isLeader){
-        this.isLeader = isLeader;
+    private int[] searchPrey(List<int[]> preysSight){
+        if(preysSight.isEmpty()){
+        }
+        Random r = new Random();
+        return new int[]{r.nextInt(3)-1, r.nextInt(3)-1};
+//        else
+//            return new int[]{-preysSight.get(0)[0], -preysSight.get(0)[1]};
+    }
+
+    public void findAlpha(){
+        if(!BlackBoard.isAssigned){
+            this.isLeader = true;
+            BlackBoard.isAssigned = true;
+        }
     }
 
     @Override
     public int[] moveAll(List<int[]> wolvesSight, List<int[]> preysSight) {
-        if(isLeader){
-            if(!blackBoard.getIsAssigned()){
-//                blackBoard.setIsAssigned(true);
-                blackBoard.setRelDistWolf1(wolvesSight.get(0));
-                blackBoard.setRelDistWolf2(wolvesSight.get(1));
+        findAlpha();
+        if(this.isLeader){
+            List<int[]> relDistWolves = new ArrayList<>();
+            for(int i=0; i<wolvesSight.size(); i++){
+                int[] wolfRel = wolvesSight.get(i);
+                if(!(wolfRel[0]==0 && wolfRel[1]==0)){
+                    relDistWolves.add(wolfRel);
+                }
             }
-        }
+            blackBoard.setRelDistWolves(relDistWolves);
+            return searchPrey(preysSight);
+        }else {
+            int[] nextMove = new int[2];
+            List<int[]> relDistWolves = blackBoard.getRelDistWolves();
+            for (int i = 0; i < wolvesSight.size(); i++) {
+                for (int j = 0; j < relDistWolves.size(); j++) {
+                    int[] relDistWolf = relDistWolves.get(j);
+                    int[] wolfSight = wolvesSight.get(i);
+                    if (wolfSight[0] == -relDistWolf[0] && wolfSight[1] == -relDistWolf[1]) {
+                        for (int k = 0; k < nextMove.length; k++) {
+                            if (wolfSight[k] > 0) {
+                                nextMove[k] = -1;
+                            } else if (wolfSight[k] < 0) {
+                                nextMove[k] = 1;
+                            } else {
+                                nextMove[k] = 0;
+                            }
+                        }
 
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(nextMove));
+            return nextMove;
+        }
     }
 
     @Override
